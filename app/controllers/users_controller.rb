d538229cb35b5ccd53ae27e75dff062602f606a8
ota <124463672+kawai-ota@users.noreply.github.com>
2024-10-19 06:58:06 +0000

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+                                        
   def index
-    @users = User.paginate(page:params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -35,20 +38,28 @@ class UsersController < ApplicationController
       flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'edit'
+      render 'edit', status: :unprocessable_entity
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
-    if current_user.admin?
-      @user.destroy
-      flash[:success] = "User deleted"
-      redirect_to users_url, status: :see_other
-    else
-      flash[:danger] = "You are not authorized to perform this action"
-      redirect_to root_url, status: :see_other
-    end
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
@@ -60,20 +71,10 @@ class UsersController < ApplicationController
 
     # beforeフィルター
     
-    #ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-
-
     # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     # 管理者かどうかを確認
